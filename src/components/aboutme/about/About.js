52938d9e22fd603e5a071a289739e4bbf90de1d4
CodeNinja660 <nagaomasaki660@gmail.com>
2024-01-15 19:08:56 -0800

    I've modified my portfolio.

--- src/components/aboutme/about/About.js
+++ src/components/aboutme/about/About.js
@@ -28,36 +28,36 @@ const About = () => {
           <Col md={7}>
             <Fade duration={3000}>
               <p className="home-about-body">
-                My name is <b className="purple">MD Mafujul Hasan </b> 
-                  and I am a <b className="purple">Computer Science and Engineering </b> 
-                    student at <b className="purple">Sonargaon University </b>
-                      in Dhaka, Bangladesh.
+                My name is <b className="purple">Nagao Masaki </b>
+                and I am a <b className="purple">Computer Science and Engineering </b>
+                student at <b className="purple">Sonargaon University </b>
+                in Dhaka, Bangladesh.
                 <br />
                 <br />
-                        I am proficient in various programming languages such as &nbsp;
-                  <b className="purple">
-                    {" "}
-                    C, C++, Java, Python, HTML, CSS, PHP, and JavaScript.{" "}
-                  </b>
+                I am proficient in various programming languages such as &nbsp;
+                <b className="purple">
+                  {" "}
+                  C, C++, Java, Python, HTML, CSS, PHP, and JavaScript.{" "}
+                </b>
                 <br />
                 <br />
                 I also have experience working with frameworks and libraries such as &nbsp;
 
-                  <b className="purple">
-                    {" "}
-                    NodeJS, ReactJS, OpenCV, Pandas, and NumPy.{" "}
-                  </b>
+                <b className="purple">
+                  {" "}
+                  NodeJS, ReactJS, OpenCV, Pandas, and NumPy.{" "}
+                </b>
                 <br />
                 <br />
                 Whenever possible, I also apply my passion for developing
                 products with &nbsp;
-                  <b className="purple">
-                    {" "}
-                    Modern Javascript Library and Frameworks.
-                  </b>
+                <b className="purple">
+                  {" "}
+                  Modern Javascript Library and Frameworks.
+                </b>
                 <br />
                 <br />In my free time, I enjoy learning new technologies and &nbsp;
-                  <b className="purple">building new web technologies and products. </b>
+                <b className="purple">building new web technologies and products. </b>
               </p>
             </Fade>
           </Col>
